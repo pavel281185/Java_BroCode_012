d85b1877a1d81d48e229f4b8a220b2342ec63ff9
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 14:10:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // while loop
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isBlank()){
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        do {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }while (name.isBlank());
+
+        System.out.println("Hello " + name);
+    }
+}
